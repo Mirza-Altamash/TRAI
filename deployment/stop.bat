4cb68858c9ce2f3ec037066f344ac86f5ba91d95
Mirza Altamash <your.email@example.com>
2026-07-08 14:46:44 +0530

    fix: syntax in stop.bat

--- deployment/stop.bat
+++ deployment/stop.bat
@@ -1,5 +1,4 @@
 @echo off
-for /f "tokens=5" %%a in ('netstat -ano ^^| findstr :8085') do taskkill /PID %%a /F
-for /f "tokens=5" %%a in ('netstat -ano ^^| findstr :5002') do taskkill /PID %%a /F
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8085') do taskkill /PID %%a /F
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :5002') do taskkill /PID %%a /F
 echo Stopped processes on ports 8085 and 5002.
-pause
